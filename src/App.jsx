d0ed063ccf126ed7dc81865f9b87620612344355
talhaahussein-design <talhaahussein@gmail.com>
2026-06-29 00:37:26 +0200

    Set up routing with React Router in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
-import "./index.css";
+import Feelings from "./pages/Feelings";
+import BodyMap from "./pages/BodyMap";
+import Communication from "./pages/Communication";
+import Stories from "./pages/Stories";
+import Profile from "./pages/Profile";
 
 export default function App() {
-  return <Home />;
+  return (
+    <BrowserRouter basename="/St-tteguide">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/feelings" element={<Feelings />} />
+        <Route path="/bodymap" element={<BodyMap />} />
+        <Route path="/communication" element={<Communication />} />
+        <Route path="/stories" element={<Stories />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
