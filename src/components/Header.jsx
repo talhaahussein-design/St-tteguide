5a6356b4affdcc6cdc7f3e448c3c1ff1d6f6f7ec
talhaahussein-design <talhaahussein@gmail.com>
2026-06-28 07:39:17 +0200

    Add Header component with logo and title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,20 @@
+export default function Header() {
+  return (
+    <header className="header">
 
+      <div className="logo">
+        🌿
+      </div>
+
+      <div>
+        <h3>Støtteguiden</h3>
+        <span>Din digitale støtte</span>
+      </div>
+
+      <button className="profileButton">
+        👤
+      </button>
+
+    </header>
+  );
+}
