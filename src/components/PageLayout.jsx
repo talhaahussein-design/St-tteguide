92d0cba355a2b54e9197487441ed7a19be82e7b8
talhaahussein-design <talhaahussein@gmail.com>
2026-06-29 00:44:36 +0200

    Add PageLayout component with header and navigation

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,1 +1,26 @@
+import { Link } from "react-router-dom";
+import BottomNav from "./BottomNav";
 
+export default function PageLayout({ title, children }) {
+  return (
+    <main className="page">
+
+      <header className="pageHeader">
+
+        <Link to="/" className="backButton">
+          ←
+        </Link>
+
+        <h1>{title}</h1>
+
+      </header>
+
+      <section className="pageContent">
+        {children}
+      </section>
+
+      <BottomNav />
+
+    </main>
+  );
+}
