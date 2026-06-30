862cac18af30a12a94cb433c8d5403db63f0169b
talhaahussein-design <talhaahussein@gmail.com>
2026-06-30 04:16:52 +0200

    Update ParentLayout.jsx

--- src/features/parent/ParentLayout.jsx
+++ src/features/parent/ParentLayout.jsx
@@ -1,1 +1,9 @@
-
+export default function ParentLayout({ children }) {
+  return (
+    <div className="page-container">
+      <div className="parent-wrapper">
+        {children}
+      </div>
+    </div>
+  );
+}
